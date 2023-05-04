a52cff91df72e60fe79d7d1204f885fc9219da58
Stark561 <123174593+Stark561@users.noreply.github.com>
2023-05-04 12:00:58 +0000

    Deploying to gh-pages from @ Stark561/goit-js-hw-09@80a3b79e7d54eea9301a915bcbb6e099a9c75250 🚀

---
+++ 01-color-switcher.e66730fd.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.body;t.addEventListener("click",(()=>{intervalId=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;a.style.backgroundColor=t}),1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(intervalId),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.e66730fd.js.map
