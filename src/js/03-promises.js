0e2d9e90a6c1b3a65f2e19af7a5f4346efea3991
Stark561 <123174593+Stark561@users.noreply.github.com>
2023-05-04 21:46:18 +0300

    hw-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const formLink = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+formLink.addEventListener('submit', evt => {
+  evt.preventDefault();
+  const inputedAmount = Number(evt.currentTarget.elements.amount.value);
+  let firstDelay = Number(evt.currentTarget.elements.delay.value);
+  const stepDelay = Number(evt.currentTarget.elements.step.value);
+
+  for (let i = 1; i <= inputedAmount; i++) {
+    createPromise(i, firstDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    firstDelay += stepDelay;
+  }
+  formLink.reset();
+});
